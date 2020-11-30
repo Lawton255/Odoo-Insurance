5a501fd5e5bb3f1961152f8fecc46dd5f481f3c3
Lawton255 <elawton255@gmail.com>
2020-12-01 02:02:56 +0300

    insurer type added

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@
 
 from . import models
 from . import insurance
+from . import supplier
--- models/insurance.py
+++ models/insurance.py
@@ -3,6 +3,7 @@ from odoo import models, fields, api
 class insurance(models.Model):
     _name = 'insurance.insurance'
     _rec_name = 'truck'
+    _order    = 'exp_date asc'
 
     @api.multi
     def button_expire(self):
@@ -14,10 +15,14 @@ class insurance(models.Model):
         for rec in self:
             rec.write({'state': 'closed'})
 
-    certificate_no = fields.Integer(string='Insurance Certificate number', unique=True, required=True)
-    truck          = fields.Many2one('fleet.vehicle', string='Truck',required=True)
-    insurer        = fields.Many2one('res.partner', string='Insurer' , required=True)
-    amount      = fields.Integer(string="Amount", required=True)
+    licence_no  = fields.Char(string="Licence no")
+    receipt_no  = fields.Char(string="Receipt no", required=True)
+    truck          = fields.Many2one('fleet.vehicle', string='Truck hose',required=True)
+    supplier_name  = fields.Selection([('insurance', 'Insurance'), ('sumatra', 'Sumatra'), 
+    ('c28', 'C28'), ('comesa', 'Comesa'), ('carbon_tax', 'Carbon Tax'), ('permit_tax', 'Permit Tax')], required=True)
+    amount      = fields.Float(string="Amount", required=True)
+    paid_by     = fields.Many2one('res.partner', string="Paid by" , required=True)
+    paid        = fields.Boolean(string="Paid", default=True , required=True)
     commence_date  = fields.Date(string='Start Date' , required=True)
     exp_date       = fields.Date(string='Expiry Date' , required=True)
 
